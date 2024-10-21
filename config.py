e6918236d854e780dd47c8342f9ee277c062494d
Ray <raym_do@hotmail.com>
2024-10-21 11:23:01 +0200

    config file adjustment

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'md0WJsjaOOuf1T')
-
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'md0WJsjaOOuf1T'
+    FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
+    
+# Je kunt ook een configuratieklasse gebruiken als je meer omgevingen hebt, zoals DevelopmentConfig of ProductionConfig.
